4ba723d436080bbcb71320660637430ef9dfd5f6
cbarr14 <cbarr14@bancodebogota.com.co>
2024-10-27 15:24:56 -0500

    first commint

---
+++ build/src/models/IDisbursements.d.ts
@@ -0,0 +1,13 @@
+export interface IDisbursements {
+    disbursement_id: string;
+    request_id: string;
+    identification_number: string;
+    date: string;
+    request_json: string;
+    response_json: string;
+    status: string;
+    credit_number: string | null;
+    amount: number;
+    term: number;
+    rate: number;
+}
---
+++ build/src/models/IDisbursements.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=IDisbursements.js.map
