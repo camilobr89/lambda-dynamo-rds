68e4ca1ef10350971131eb816c71c2540f5ec5c4
cbarr14 <cbarr14@bancodebogota.com.co>
2024-10-28 17:21:04 -0500

    first commint

--- src/utils/maskCreditNumber.ts
+++ src/utils/maskCreditNumber.ts
@@ -1,5 +1,5 @@
 export const maskCreditNumber = (creditNumber: string): string | null => {
     if (!creditNumber || creditNumber.length < 4) return null;
-    return '****' + creditNumber.slice(-4);
+    return '*****' + creditNumber.slice(-4);
   };
   
